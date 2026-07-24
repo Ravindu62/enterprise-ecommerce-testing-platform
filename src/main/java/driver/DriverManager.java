a573271b46fc299e844f8373f5d5a58cc147dd1e
Ravindu62 <pasan62nanayakkara@gmail.com>
2026-07-24 13:57:11 +0530

    feature/automation - add driver package. under driver add BrowserFactory, DriverFactory, DriverManager classes

---
+++ src/main/java/driver/DriverManager.java
@@ -0,0 +1,36 @@
+package driver;
+
+import org.openqa.selenium.WebDriver;
+
+
+public class DriverManager {
+
+
+    private static final ThreadLocal<WebDriver>
+            driver = new ThreadLocal<>();
+
+
+
+    public static void setDriver(WebDriver webDriver) {
+
+        driver.set(webDriver);
+
+    }
+
+
+
+    public static WebDriver getDriver() {
+
+        return driver.get();
+
+    }
+
+
+
+    public static void unloadDriver() {
+
+        driver.remove();
+
+    }
+
+}
