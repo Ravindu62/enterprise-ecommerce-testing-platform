2501b75874e4172dc6e38cb7944ca14ec17fb652
Ravindu62 <pasan62nanayakkara@gmail.com>
2026-07-24 14:19:22 +0530

    feature/automation - add data, file, logger and test data util files to the util package

---
+++ src/main/java/utils/LoggerUtils.java
@@ -0,0 +1,22 @@
+package utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class LoggerUtils {
+
+
+    private LoggerUtils() {
+
+    }
+
+
+    public static Logger getLogger(Class<?> clazz) {
+
+        return LoggerFactory.getLogger(clazz);
+
+    }
+
+
+}
